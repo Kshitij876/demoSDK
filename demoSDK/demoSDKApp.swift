6c7ef55965e17789b729696117515b6220eb8770
Kshitij876 <kshitij.bhosale@thinkitive.com>
2023-06-02 17:18:27 +0530

    Initial Commit

---
+++ demoSDK/demoSDKApp.swift
@@ -0,0 +1,17 @@
+//
+//  demoSDKApp.swift
+//  demoSDK
+//
+//  Created by Thinkitive on 02/06/23.
+//
+
+import SwiftUI
+
+@main
+struct demoSDKApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
